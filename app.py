fc0f730ec9ef1cbc5760ea6d7454f738354f2c9d
bflisk <77073085+bflisk@users.noreply.github.com>
2021-04-20 16:21:33 -0400

    authorization flow done
    - Completed the authorization flow for the user
    -  TODO: Have user's tracks display using the getTracks() function
    - BUG: token can be initially granted, but for some reason is not refreshed

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, url_for
@@ -20,14 +21,13 @@ SPOTIPY_CLIENT_SECRET = environ.get('SPOTIPY_CLIENT_SECRET')
 
 # Creates secret key and names session cookie
 app.secret_key = "GJOsgojhG08u9058hSDfj"
-app.config["SESSION_COOKIE_NAME"] = "Brendan's Cookie"
+app.config["SESSION_COOKIE_NAME"] = "SpotiHelp Cookie"
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 # Application
-
 
 # Homepage
 @app.route("/", methods=["GET", "POST"])
@@ -51,6 +51,7 @@ def index():
         return render_template("index.html")
 
 
+# Has the user log in and authorize SpotiHelp to use information from Spotify
 @app.route("/login")
 def login():
     sp_oauth = create_spotify_oauth() # Creates a new sp_oauth object everytime a user logs in
@@ -61,24 +62,54 @@ def login():
 
 @app.route("/redirect")
 def redirectPage():
-    return "redirect"
+    sp_oauth = create_spotify_oauth()
+    session.clear() # Gets rid of current session
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session["token_info"] = token_info # Saves token info into the the session
+    print(session["token_info"])
+
+    # Tries to get token data. If it doesn't succeed, returns user back to index page
+    try:
+        token_info = get_token()
+    except:
+        print("-- User not logged in! --")
+        return redirect(url_for("index", _external=False))
+
+    return redirect(url_for("index", _external=False))
 
 
-@app.route("/getTracks")
+# Gets user tracks
+# TODO Require user log-in
+@app.route("/get-tracks")
 def getTracks():
-    return "Get Tracks TODO"
+    # Tries to get token data. If it doesn't succeed, returns user back to index page
+    try:
+        token_info = get_token()
+    except:
+        print("-- User not logged in! --")
+        return redirect(url_for("index", _external=False))
+
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+    saved_tracks = sp.current_user_saved_tracks(limit=1, offset=0)
+
+    #return render_template("get-tracks.html", saved_tracks=saved_tracks)
+    return saved_tracks['items']['track']['name']
 
 
+# Allows the user to create new smart playlists
 @app.route("/playlist-new", methods=["GET", "POST"])
 def new_playlist():
     return render_template("playlist-new.html")
 
 
+# Allows the user to edit existing playlists
 @app.route("/playlist-edit", methods=["GET", "POST"])
 def edit_playlist():
     return render_template("playlist-edit.html")
 
 
+# Creates a spotify authorization object
 def create_spotify_oauth():
     return SpotifyOAuth(
         client_id=SPOTIPY_CLIENT_ID,
@@ -88,5 +119,25 @@ def create_spotify_oauth():
         user-top-read app-remote-control playlist-modify-public user-modify-playback-state playlist-modify-private
         user-follow-modify user-read-currently-playing user-follow-read user-library-modify
         user-read-playback-position playlist-read-private user-read-email user-read-private
-        user-library-read playlist-read-collaborative streaming""",
-        show_dialog=True)
+        user-library-read playlist-read-collaborative streaming""")
+
+
+# Processes the session token and refreshes it if needed
+def get_token():
+    token_info = session.get("token_info", None) # Gives token information if it exists, otherwise given 'None'
+
+    # Checks if token_info is 'None'
+    if not token_info:
+        return redirect(url_for("index", _external=True))
+    else:
+        return token_info
+    
+    now = int(time.time()) # Gets current time
+    is_expired = token_info['expires_at'] - now < 60 # T or F depending on condition
+
+    # If the token is close to expiring, refresh it
+    if is_expired:
+        sp_oauth = create_spotify_oauth() 
+        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+
+    return token_info
