31f41461cd950bb0190a524ffd4817953f7137b0
bflisk <77073085+bflisk@users.noreply.github.com>
2021-04-20 13:41:10 -0400

    creating redirect.html

--- app.py
+++ app.py
@@ -84,5 +84,9 @@ def create_spotify_oauth():
         client_id=SPOTIPY_CLIENT_ID,
         client_secret=SPOTIPY_CLIENT_SECRET,
         redirect_uri=url_for('redirectPage',_external=True),
-        scope="user-library-read",
+        scope="""user-library-read ugc-image-upload user-read-recently-played user-read-playback-state 
+        user-top-read app-remote-control playlist-modify-public user-modify-playback-state playlist-modify-private
+        user-follow-modify user-read-currently-playing user-follow-read user-library-modify
+        user-read-playback-position playlist-read-private user-read-email user-read-private
+        user-library-read playlist-read-collaborative streaming""",
         show_dialog=True)
