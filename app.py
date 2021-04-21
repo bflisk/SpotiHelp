78f818bde39e6b5b87f1da9e22ea57929352a53c
bflisk <77073085+bflisk@users.noreply.github.com>
2021-04-21 17:43:30 -0400

    List user songs
    - added listing all user songs using getTracks()
    - fixed "refresh token revoked" by having the server delete the .cache file every time a user logs in

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+import path
 
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, url_for
@@ -25,9 +26,11 @@ app.config["SESSION_COOKIE_NAME"] = "SpotiHelp Cookie"
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+sp_oauth = None
 
 
 # Application
+# Routable functions
 
 # Homepage
 @app.route("/", methods=["GET", "POST"])
@@ -54,7 +57,14 @@ def index():
 # Has the user log in and authorize SpotiHelp to use information from Spotify
 @app.route("/login")
 def login():
-    session.clear()
+    # Removes the cache file
+    try:
+        print("_____________________CACHE REMOVED_____________________")
+        os.remove(r"C:\Users\Sylux\Desktop\Desktop\Code\WebDev\SpotiHelp\.cache")
+    except:
+        pass
+
+    session.clear() # Gets rid of current session
     sp_oauth = create_spotify_oauth() # Creates a new sp_oauth object everytime a user logs in
     auth_url = sp_oauth.get_authorize_url() # Passes the authorization url into a variable
 
@@ -63,12 +73,12 @@ def login():
 
 @app.route("/redirect")
 def redirectPage():
-    sp_oauth = create_spotify_oauth()
-    session.clear() # Gets rid of current session
+    sp_oauth = create_spotify_oauth() # Creates a new sp_oauth object
+
+    # Gets token information from the response 
     code = request.args.get('code')
     token_info = sp_oauth.get_access_token(code)
     session["token_info"] = token_info # Saves token info into the the session
-    print(session["token_info"])
 
     # Tries to get token data. If it doesn't succeed, returns user back to index page
     try:
@@ -89,13 +99,29 @@ def getTracks():
         token_info = get_token()
     except:
         print("-- User not logged in! --")
-        return redirect(url_for("index", _external=False))
+        return redirect("/login")
 
-    sp = spotipy.Spotify(auth=token_info['access_token'])
-    saved_tracks = sp.current_user_saved_tracks(limit=1, offset=0)
+    # Accesses user's saved tracks using access token
+    pageNum = request.form.get("page", 0) # Gets page number user wants to be on
+    """if pageNum < 0:
+        pageNum = 0"""
+    
+    saved_tracks = []
+    iteration = 0
+    try:
+        sp = spotipy.Spotify(auth=token_info['access_token'])
+    except:
+        return redirect("/login")
+    
+    while True:
+        items = sp.current_user_saved_tracks(limit=50, offset=iteration)['items']
+        iteration += 50
+        saved_tracks.append(items) # Appends the list with all of the user's saved tracks
+        if len(items) < 50: # Breaks out of loop when it reaches the end of the user's library
+            break
 
-    #return render_template("get-tracks.html", saved_tracks=saved_tracks)
-    return saved_tracks['items']['track']['name']
+    return render_template("get-tracks.html", saved_tracks=saved_tracks, pageNum=pageNum)
+    #return len(saved_tracks)
 
 
 # Allows the user to create new smart playlists
@@ -110,20 +136,22 @@ def edit_playlist():
     return render_template("playlist-edit.html")
 
 
+# Non-routable functions
+
 # Creates a spotify authorization object
 def create_spotify_oauth():
     return SpotifyOAuth(
         client_id=SPOTIPY_CLIENT_ID,
         client_secret=SPOTIPY_CLIENT_SECRET,
         redirect_uri=url_for('redirectPage',_external=True),
-        scope="""user-library-read ugc-image-upload user-read-recently-played user-read-playback-state 
-        user-top-read app-remote-control playlist-modify-public user-modify-playback-state playlist-modify-private
-        user-follow-modify user-read-currently-playing user-follow-read user-library-modify
-        user-read-playback-position playlist-read-private user-read-email user-read-private
-        user-library-read playlist-read-collaborative streaming""")
+        scope="""ugc-image-upload user-read-recently-played user-read-playback-state 
+        user-top-read app-remote-control playlist-modify-public user-modify-playback-state 
+        playlist-modify-private user-follow-modify user-read-currently-playing user-follow-read 
+        user-library-modify user-read-playback-position playlist-read-private user-read-email 
+        user-read-private user-library-read playlist-read-collaborative streaming""")
 
 
-# Processes the session token and refreshes it if needed
+# Returns a valid access token, refreshing it if needed
 def get_token():
     token_info = session.get("token_info", None) # Gives token information if it exists, otherwise given 'None'
 
@@ -133,6 +161,7 @@ def get_token():
     else:
         return token_info
     
+    # Checks if token is close to expiring
     now = int(time.time()) # Gets current time
     is_expired = token_info['expires_at'] - now < 60 # T or F depending on condition
 
