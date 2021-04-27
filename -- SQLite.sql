51746e9a9c00fc15dc42872ec6f7062e27bddc5a
bflisk <77073085+bflisk@users.noreply.github.com>
2021-04-27 16:10:40 -0400

    LESS GOOOO
    - Fixed the login refresh token issue
    - User can now look at their created playlists and browse through them via edit_playlist()

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -1,8 +1,10 @@
 -- SQLite
 CREATE TABLE users (id INTEGER NOT NULL, username TEXT NOT NULL, PRIMARY KEY(id));
+CREATE TABLE playlists (user_id INTEGER NOT NULL, playlist_id TEXT NOT NULL, playlist_name TEXT NOT NULL, playlist_art TEXT NOT NULL, playlist_link TEXT NOT NULL, PRIMARY KEY(playlist_id));
 
-INSERT INTO users (username) VALUES ('testuser2');
+INSERT INTO playlists (user_id, playlist_id, playlist_name) VALUES ('syluxhunter1352', "1AVZz0mBuGbCEoNRQdYQju", 'A Playlist');
 
 SELECT * FROM users;
 
-DROP TABLE users;
+DELETE FROM playlists WHERE user_id=1;
+DROP TABLE playlists;
--- app.py
+++ app.py
@@ -72,24 +72,17 @@ def create_spotify_oauth():
 # Returns a valid access token, refreshing it if needed
 def get_token():
     token_info = session.get("token_info", None) # Gives token information if it exists, otherwise given 'None'
-    print("PING PING PING")
 
-    # Checks if token_info is 'None'
-    """if not token_info:
-        print("FUCK NIGGA")
-        return redirect(url_for("index"))
-    """
+    # Checks if token_info is 'None' and redirects user to login
+    if not token_info:
+        return redirect(url_for("login", _external=True))
 
     # Checks if token is close to expiring
     now = int(time.time()) # Gets current time
-    print("slkdjfghlskdfjhgslkdjfghslkjdfghlskjdfhglskdjfhglksjhdfglksdjhfg")
-    print(token_info)
-    is_expired = token_info['expires_at'] - now < 60 # T or F depending on condition
-    print(now)
+    is_expired = (token_info['expires_at'] - now) < 60 # T or F depending on condition
 
     # If the token is close to expiring, refresh it
     if is_expired:
-        print("TOKEN EXPIRED")
         sp_oauth = create_spotify_oauth() 
         token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
 
@@ -113,14 +106,12 @@ def create_sp():
     # Tries to get token data. If it doesn't succeed, returns user back to index page
     try:
         token_info = get_token()
-        print("token")
         sp = spotipy.Spotify(auth=token_info['access_token'])
-        print("sp object")
     except:
-        print('create token failure')
         return redirect(url_for("login")) 
 
     return sp
+
 
 # --- Routable functions ---
 
@@ -132,9 +123,6 @@ def login():
 
         sp_oauth = create_spotify_oauth() # Creates a new sp_oauth object everytime a user logs in
         auth_url = sp_oauth.get_authorize_url() # Passes the authorization url into a variable
-        print("LOOK HERE YOU FUCK")
-        print(sp_oauth)
-        #token_info = sp_oauth.get_access_token(sp_oauth)
 
         return redirect(auth_url) # Redirects user to the authorization url
     else:
@@ -165,41 +153,23 @@ def index():
 @app.route("/redirect")
 def redirectPage():
     sp_oauth = create_spotify_oauth() # Creates a new sp_oauth object
-
-    # Gets token information from the response 
-    code = request.args.get('code')
-    # token_info = sp_oauth.get_access_token(code)
-    token_info = get_token()
+    session.clear()
+    
+    code = request.args.get('code') # Gets code from response URL
+    token_info = sp_oauth.get_access_token(code) # Uses code sent from Spotify to exchange for an access & refresh token
     session["token_info"] = token_info # Saves token info into the the session
 
-    # sp = spotipy.Spotify(auth=token_info['access_token']) # Parses spotify response information
+    # Gets current user's identifying information
     sp = create_sp()
-    print("--------------------------------------------------------------------")
-    print(sp)
-    print("--------------------------------------------------------------------")
-    sp = jsonify(sp)
 
-    try:
-        username = sp.current_user()['display_name'] # Gets current user's username
-    except:
-        return redirect(url_for("index"))
+    # Passes identifying info into user's session
+    session["username"] = sp.current_user()['display_name'] # Sets session username
+    session["user_id"] = db.execute("SELECT id FROM users WHERE username=?", session["username"])[0]['id'] # Sets session user id
 
     # Checks if the user is registered and adds to database accordingly
-    if not db.execute("SELECT * FROM users WHERE username=?", username):
-        db.execute('INSERT INTO users (username) VALUES (?);', username)
-
-    session["user_id"] = db.execute("SELECT id FROM users WHERE username=?", username) # Sets session user id
-    session["username"] = username # Sets session username
-    
-    # print(sp.current_user())
-    
-
-    # Tries to get token data. If it doesn't succeed, returns user back to index page
-    try:
-        token_info = get_token()
-    except:
-        return redirect(url_for("index", _external=True))
-
+    if not db.execute("SELECT * FROM users WHERE username=?", session["username"]):
+        db.execute('INSERT INTO users (username) VALUES (?);', session["username"])
+        
     return redirect(url_for("index", _external=True))
 
 
@@ -208,7 +178,6 @@ def redirectPage():
 @login_required
 def logout():
     clear_session()
-
     return redirect(url_for("index"))
 
 
@@ -218,7 +187,7 @@ def logout():
 def getTracks():
     sp = create_sp() # Creates a new spotify object 
     total_tracks = sp.current_user_saved_tracks(limit=1, offset=0)['total'] # Total tracks user has saved
-    batch = 20 # Amounts of tracks to show per page
+    result_size = 20 # Amounts of tracks to show per page
 
     if request.method == "POST": 
         pageNum = session["track_pageNum"] # Gets the current page number
@@ -231,18 +200,18 @@ def getTracks():
         # Makes sure page number is valid
         if pageNum < 0:
             pageNum = 0
-        elif pageNum > total_tracks/batch:
-            pageNum = int(total_tracks/batch) 
+        elif pageNum > total_tracks/result_size:
+            pageNum = int(total_tracks/result_size) 
         
         session["track_pageNum"] = pageNum # Sets the session's page number       
-        saved_tracks = sp.current_user_saved_tracks(limit=batch, offset=pageNum * batch)['items'] # Retrieves the designated page of user tracks
+        saved_tracks = sp.current_user_saved_tracks(limit=result_size, offset=pageNum * result_size)['items'] # Retrieves the designated page of user tracks
 
         return render_template("get-tracks.html", saved_tracks=saved_tracks, total_tracks=total_tracks, pageNum=pageNum)
     else:
         pageNum = 0 # Default value for initially loaded page
         session["track_pageNum"] = pageNum # Sets the session's page number
 
-        saved_tracks = sp.current_user_saved_tracks(limit=batch, offset=0)['items'] # Retrieves the designated page of user tracks
+        saved_tracks = sp.current_user_saved_tracks(limit=result_size, offset=0)['items'] # Retrieves the designated page of user tracks
 
         return render_template("get-tracks.html", saved_tracks=saved_tracks, total_tracks=total_tracks, pageNum=pageNum)
 
@@ -257,10 +226,54 @@ def new_playlist():
 
 
 # Allows the user to edit existing playlists
-@app.route("/playlist-edit")
+@app.route("/playlist-edit", methods=["GET", "POST"])
 @login_required
 def edit_playlist():
-    sp = create_sp() # Creates a new spotify object
-    playlists = sp.user_playlists(session["username"])
+    if request.method == "POST":
+        sp = create_sp()
+        playlist_id = request.form.get("playlist")
+        temp = sp.playlist(playlist_id)
+        playlist = {'playlist_id': playlist_id, 'playlist_name': temp['name'], 
+                    'playlist_art': temp['images'][0]['url'], 'playlist_link': temp['external_urls']['spotify'], 
+                    'tracks': []}
+        
+        for i in temp['tracks']['items']:
+            playlist['tracks'].append(i)
+            print(playlist['tracks'])
 
-    return render_template("playlist-edit.html")
+        return render_template("playlist-edit.html", playlists=None, playlist=playlist, specific_playlist=True)
+    else:
+        # Gets the user's playlists
+        sp = create_sp()
+        playlists = []
+        
+        # Checks whether the user's playlists have been saved in the database yet and retrieves them
+        if not db.execute("SELECT * FROM playlists WHERE user_id=?;", session['user_id']):
+            # Gets initial batch of playlists from spotify
+            i = 0
+            batch = sp.current_user_playlists(limit=50, offset=i*50)['items']
+
+            # Loops through all user-saved playlists 50 at a time
+            while batch:
+
+                # Loops through current batch of playlists and adds user-created playlists into database
+                for playlist in batch:
+                    if playlist['owner']['id'] == session['username']:
+                        # Tries to get playlist art
+                        try:
+                            playlist_art = playlist['images'][0]['url']
+                        except:
+                            playlist_art = ''
+
+                        # Adds playlist into database
+                        db.execute("INSERT INTO playlists VALUES (?,?,?,?,?);", session['user_id'], playlist['id'], playlist['name'], 
+                                  playlist_art, playlist['external_urls']['spotify'])  
+
+                # Continues iterating over all user playlists  
+                i += 1
+                batch = sp.current_user_playlists(limit=50, offset=i*50)['items']
+
+        # Retrieves user's playlists from database       
+        playlists = db.execute("SELECT * FROM playlists WHERE user_id=?;", session['user_id'])
+
+        return render_template("playlist-edit.html", playlists=playlists, playlist=None, specific_playlist=False)
